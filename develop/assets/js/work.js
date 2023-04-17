e47125990fa6056dc8c33a709e13136598d380ab
Inna Grant <i.grant@ficto.ru>
2023-04-17 22:42:52 +0300

    .....

---
+++ develop/assets/js/work.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+   /* читать дальше */
+   
+   if ($('.project-is').length > 0) {
+      /*$(".map").mousemove(function (pos) {
+         console.log('Относительные координаты: По оси X:'+pos.offsetX+', По оси Y:'+pos.offsetY);
+      });*/
+      
+      $(".map .region").on( 'click', function (event) {
+         let target = event.target,
+             id = $(target).prop('id'),
+             $img = $('.project__map_img'),
+             hide = 'region_hide';
+         
+         $('.region_hide').removeClass(hide);
+         $img.removeProp('class').addClass('project__map_img');
+         $img.addClass(id);
+         $('.region').addClass(hide);
+         
+         $img.on( 'click', function(e) {
+            console.log(e.target);
+            $('.region_hide').removeClass(hide);
+            $(this).removeClass(id);
+            // console.log($(this).removeProp('class'));
+         })
+         
+         
+         
+         // console.log('Относительные координаты: По оси X:' + e.offsetX+', По оси Y:' + e.offsetY);
+      });
+    
+
+   }
+   
+   
+});
