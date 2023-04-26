5f22dc67862ed1e7f75432706b954aea3ef6912f
Inna Grant <i.grant@ficto.ru>
2023-04-26 16:16:43 +0300

    .....

--- build/assets/js/work.js
+++ build/assets/js/work.js
@@ -1,1 +1,46 @@
-$(document).ready(function(){0<$(".project-is").length&&($(".map .region").on("click",function(e){let o=e.target,r=$(o).prop("id"),i=$(".project__map_img"),_="region_hide";$(".region_hide").removeClass(_),i.removeProp("class").addClass("project__map_img"),i.addClass(r),$(".region").addClass(_);let a=$(".project__region_holder.card_"+r);a.fadeIn(),i.on("click",function(e){$(".region_hide").removeClass(_),$(this).removeClass(r),$(".project__region_holder").fadeOut()})}),$(".project__region_names div").on("click",function(e){let o=e.target,r=$(o).data("region"),i=$(".project__map_img");$(".region_chosen").removeClass("region_chosen"),$('.project__region_holder[ id^="card_big-"]').fadeOut(),$(".project__region_holder.card_"+r+'[ id^="card_big-"]').fadeIn(),$(this).addClass("region_chosen"),i.removeAttr("class").addClass("project__map_img"),i.addClass(r),i.on("click",function(e){$(".region_chosen").removeClass("region_chosen"),$('.project__region_holder[ id^="card_big-"]').fadeOut(),$(this).removeClass(r)})}))});
+$(document).ready(function () {
+   
+   if ($('.project-is').length > 0) {
+      
+      $(".map .region").on( 'click', function (event) {
+         let target = event.target,
+             id = $(target).prop('id'),
+             $img = $('.project__map_img'),
+             hide = 'region_hide';
+         
+         $('.region_hide').removeClass(hide);
+         $img.removeProp('class').addClass('project__map_img');
+         $img.addClass(id);
+         $('.region').addClass(hide);
+
+         let info = $('.project__region_holder.card_' + id );
+         info.fadeIn();
+         
+         $img.on( 'click', function(e) {
+            $('.region_hide').removeClass(hide);
+            $(this).removeClass(id);
+            $('.project__region_holder').fadeOut();
+         });
+      });
+      
+      $('.project__region_names div').on( 'click', function (event) {
+         let target = event.target,
+             data = $(target).data('region'),
+             $img = $('.project__map_img');
+         
+         $('.region_chosen').removeClass('region_chosen');
+         $('.project__region_holder[ id^="card_big-"]').fadeOut();
+         $('.project__region_holder.card_' + data + '[ id^="card_big-"]').fadeIn();
+         $(this).addClass('region_chosen');
+         $img.removeAttr('class').addClass('project__map_img');
+         $img.addClass(data);
+         
+         $img.on( 'click', function(e) {
+            $('.region_chosen').removeClass('region_chosen');
+            $('.project__region_holder[ id^="card_big-"]').fadeOut();
+            $(this).removeClass(data);
+         });
+      });
+      
+   }
+});
