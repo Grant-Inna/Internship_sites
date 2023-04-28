17945b29d5befb3171a01aec4bb4329763bb7333
Inna Grant <i.grant@ficto.ru>
2023-04-28 12:28:27 +0300

    .....

--- build/assets/js/work.js
+++ build/assets/js/work.js
@@ -1,1 +1,53 @@
-$(document).ready(function(){0<$(".project-is").length&&($(".map .region").on("click",function(e){let o=e.target,r=$(o).prop("id"),i=$(".project__map_img"),n="region_hide";$(".region_hide").removeClass(n),i.removeProp("class").addClass("project__map_img"),i.addClass(r),$(".region").addClass(n);let _=$(".project__region_holder.card_"+r);_.fadeIn(),i.on("click",function(e){$(".region_hide").removeClass(n),$(this).removeClass(r),$(".project__region_holder").fadeOut()})}),$(".project__region_names div").on("click",function(e){let o=e.target,r=$(o).data("region"),i=$(".project__map_img"),n=$(".project__region_names"),_=$(".project__region_holder.card_"+r+'[ id^="card_big-"]');$(".region_chosen").removeClass("region_chosen"),n.css("margin-top","0"),$('.project__region_holder[ id^="card_big-"]').fadeOut(),_.length&&(_.fadeIn(),n.css("margin-top","160px")),$(this).addClass("region_chosen"),i.removeAttr("class").addClass("project__map_img"),i.addClass(r),i.on("click",function(e){$(".region_chosen").removeClass("region_chosen"),$('.project__region_holder[ id^="card_big-"]').fadeOut(),$(this).removeClass(r),n.css("margin-top","0")})}))});
+$(document).ready(function () {
+   
+   if ($('.project-is').length > 0) {
+      
+      $(".map .region").on( 'click', function (event) {
+         let target = event.target,
+             id = $(target).prop('id'),
+             $img = $('.project__map_img'),
+             hide = 'region_hide';
+         
+         $('.region_hide').removeClass(hide);
+         $img.removeProp('class').addClass('project__map_img');
+         $img.addClass(id);
+         $('.region').addClass(hide);
+
+         let info = $('.project__region_holder.card_' + id );
+         info.fadeIn();
+         
+         $img.on( 'click', function(e) {
+            $('.region_hide').removeClass(hide);
+            $(this).removeClass(id);
+            $('.project__region_holder').fadeOut();
+         });
+      });
+      
+      $('.project__region_names div').on( 'click', function (event) {
+         let target = event.target,
+             data = $(target).data('region'),
+             $img = $('.project__map_img'),
+             $names_holder = $('.project__region_names'),
+             $card = $('.project__region_holder.card_' + data + '[ id^="card_big-"]');
+         
+         $('.region_chosen').removeClass('region_chosen');
+         $names_holder.css( 'margin-top', '0');
+         $('.project__region_holder[ id^="card_big-"]').fadeOut();
+         if ($card.length) {
+            $card.fadeIn();
+            $names_holder.css( 'margin-top', '220px');
+         }
+         $(this).addClass('region_chosen');
+         $img.removeAttr('class').addClass('project__map_img');
+         $img.addClass(data);
+         
+         $img.on( 'click', function(e) {
+            $('.region_chosen').removeClass('region_chosen');
+            $('.project__region_holder[ id^="card_big-"]').fadeOut();
+            $(this).removeClass(data);
+            $names_holder.css( 'margin-top', '0');
+         });
+      });
+      
+   }
+});
